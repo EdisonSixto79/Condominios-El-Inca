6b8a5c088674a9bce247d19b91b68b7b3965691a
EdisonSixto79 <156152639+EdisonSixto79@users.noreply.github.com>
2024-12-29 17:17:14 -0500

    wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'condominios_el_inca.settings')  # Reemplaza con el nombre de tu proyecto
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'condominios_el_inca.settings')  # Ajusta 'condominios_el_inca' si es diferente
 
 application = get_wsgi_application()
+
